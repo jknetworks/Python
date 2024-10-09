3c4c69d8aa1d0e12e8712c26e7cb955b485f287b
John Kaiser <67724664+jknetworks@users.noreply.github.com>
2024-10-09 15:03:06 -0700

    Create edit_cisco_config.py

---
+++ edit_cisco_config.py
@@ -0,0 +1,13 @@
+import netmiko
+import re
+
+csr = netmiko.ConnectHandler('10.254.0.1', username='cisco',
+   password='cisco', device_type='cisco_ios')
+
+running_config = csr.send_command('show run')
+# print(running_config)
+
+pattern = r'interface GigabitEthernet1\n.+'
+repl = 'interface GigabitEthernet1\n ip address 10.11.0.1 255.255.255.0'
+updated_config = re.sub(pattern, repl, running_config)
+print(updated_config)
